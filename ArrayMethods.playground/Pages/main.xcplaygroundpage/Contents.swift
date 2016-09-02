ccb147054aaa3bb9f1bcb7f122fb9efd9e65aa8d
jicaey <young.ma@me.com>
2016-09-02 14:22:56 -0400

    Completed Problems

--- ArrayMethods.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ ArrayMethods.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -14,69 +14,58 @@
 /*: question1
  ### 1. Create a changeable list for the five days of the week called 'daysOfTheWeek' and print each one by using a loop.
  */
-// write your code here
-
-
-
-
-
-
+var daysOfTheWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+for day in daysOfTheWeek{
+    print(day)
+}
 
 /*: question2
  ### 2. Create an unchanging list for the five days of the week called 'numDaysOfTheWeek' and print each one by using a loop, prefixed by the position of the day of the week.
  */
-// write your code here
-
-
-
-
-
-
+let numDaysOfTheWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+for (index, numDay) in numDaysOfTheWeek.enumerate(){
+    print("\(index + 1) \(numDay)")
+}
 
 /*: question3
  ### 3. Create an empty array of strings called 'emptyArray' and check to see if it's empty, printing appropriate messages.
  */
-// write your code here
+let emptyArray: [String] = []
 
-
-
-
-
+if emptyArray.isEmpty {
+    print("Empty!")
+} else {
+    print("Not empty")
+}
 
 
 
 /*: question4
  ### 4.  Create an empty array of strings called 'reverseEmptyArray' and check to see if it's empty, printing appropriate messages. Use reversed logic from question 3.
  */
-// write your code here
+var reverseEmptyArray: [String] = []
 
-
-
-
-
-
-
+if !reverseEmptyArray.isEmpty {
+    print("Not empty")
+} else {
+    print("Empty")
+}
 
 
 /*: question5
  ### 5. Find out the size of the 'daysOfTheWeek' array created earlier, and print it to the screen.
  */
-// write your code here
-
-
-
-
-
-
-
+print(daysOfTheWeek.count)
 
 /*: question6
  ### 6. Add the two days of the weekend to the 'daysOfTheWeek array and create an enumerated loop to print the values.
  */
-// write your code here
+daysOfTheWeek.append("Saturday")
+daysOfTheWeek.append("Sunday")
 
-
-
+for (index,day) in daysOfTheWeek.enumerate(){
+    print("\(index + 1) \(day)")
+}
 
 
 
@@ -84,20 +73,16 @@
 /*: question7
  ### 7. Some people start the week on a Sunday. Remove Sunday from the end of the 'daysOfTheWeek' array and then re-add it to the beginning of the array.
  */
-// write your code here
-
-
-
-
-
-
-
+daysOfTheWeek.removeAtIndex(6)
+daysOfTheWeek.insert("Sunday", atIndex: 0)
 
 /*: question8
  ### 8. Loop through the 'daysofTheWeek' array and change each value to lower case.
  */
-// write your code here
-
+for (index, day) in daysOfTheWeek.enumerate(){
+    var day = day.lowercaseString
+    print("\(index + 1) \(day)")
+}
 
 
 
@@ -108,7 +93,15 @@
 /*: question9
  ### 9. Check to see if the size of the array is greater than 5, i.e. contains the days of the weekend. If so, remove the days of the weekend from the array.
  */
-// write your code here
+func removeTheWeekend() -> [String]{
+    if daysOfTheWeek.count > 5 {
+        daysOfTheWeek.removeAtIndex(0)
+        daysOfTheWeek.removeAtIndex(5)
+    }
+    return daysOfTheWeek
+}
+
+removeTheWeekend()
 
 
 
